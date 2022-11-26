685204d68ebe786ef377377651bcf0af4fa2210c
khalil-asli <56821845+khalil-asli@users.noreply.github.com>
2022-11-26 23:23:55 +0100

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -2,4 +2,19 @@
 from main import app
 
 if __name__ == "__main__":
-        app.run()
+         if app.debug:
+                app.run()
+        else:
+                gunicorn = WSGIApplication()
+                gunicorn.load_wsgiapp = lambda: app
+                gunicorn.cfg.set('bind', '%s:%s' % (host, port))
+                gunicorn.cfg.set('workers', workers)
+                gunicorn.cfg.set('threads', workers)
+                gunicorn.cfg.set('pidfile', None)
+                gunicorn.cfg.set('worker_class', 'sync')
+                gunicorn.cfg.set('keepalive', 10)
+                gunicorn.cfg.set('accesslog', '-')
+                gunicorn.cfg.set('errorlog', '-')
+                gunicorn.cfg.set('reload', True)
+                gunicorn.chdir()
+                gunicorn.run()
